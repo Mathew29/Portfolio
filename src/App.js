d49e173cfe3bdd9414664686322c9365c621a215
Mathew Akre <mwakre29@gmail.com>
2019-11-05 11:43:20 -0800

    add route for Resume

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Welcome from './components/Welcome';
+import Resume from './components/Resume';
 
 class App extends React.Component {
   render() {
@@ -9,6 +10,7 @@ class App extends React.Component {
       <div>
         <Switch>
           <Route exact path='/' component={Welcome} />
+          <Route path='/resume' render={() => <Resume/>} />
         </Switch>
       </div>
     )
