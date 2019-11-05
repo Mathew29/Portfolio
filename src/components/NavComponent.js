fd6aeb703b066255f29008ec5da52a4332676265
Mathew Akre <mwakre29@gmail.com>
2019-11-05 13:29:38 -0800

    change navbar and add NavLink instead of Link

--- src/components/NavComponent.js
+++ src/components/NavComponent.js
@@ -1,37 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './NavComponent.css'
 
 
 class NavComponent extends React.Component {
 
-  burgerToggle() {
-    let linksE1 = document.querySelector('.narrowLinks');
-    if (linksE1.style.display === 'block') {
-      linksE1.style.display = 'none';
-    } else {
-      linksE1.style.display = 'block';
-    }
-  }
 render() {
   return(
-    <nav>
-        <div className="navWide">
-          <div className="wideDiv">
-            <Link to='/' >Link 1</Link>
-            <Link to="/">Link 2</Link>
-            <Link to="/">Link 3</Link>
-          </div>
-        </div>
-        <div className="navNarrow">
-          <i className="fa fa-bars fa-2x" onClick={this.burgerToggle}></i>
-            <div className="narrowLinks">
-              <Link to="/" onClick={this.burgerToggle}>Link 1</Link>
-              <Link to="/" onClick={this.burgerToggle}>Link 2</Link>
-              <Link to="/" onClick={this.burgerToggle}>Link 3</Link>
-            </div>
-        </div>
-      </nav>
+    <div>
+      <ul id='nav'>
+        <li><NavLink to='/' activeClassName="Link">First Link</NavLink></li>
+        <li><NavLink to='/' activeClassName="Link">Second Link</NavLink></li>
+        <li><NavLink to='/' activeClassName="Link">Third Link</NavLink></li>
+      </ul>
+    </div>
   )
 }
 }
