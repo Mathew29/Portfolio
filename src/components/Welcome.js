0b69409f246af166587db062723a1137039f6277
Mathew Akre <mwakre29@gmail.com>
2019-11-05 11:29:18 -0800

    import Link

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Welcome() {
   return (
     <div className="container">
       <h1 className="header">Hello, I'm</h1>
       <h1 className="header">Mathew Akre</h1>
-      
+
     </div>
   )
 }
