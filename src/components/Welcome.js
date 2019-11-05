f84b5a7d974d93a3e47dbe697900a9236c19e33a
Mathew Akre <mwakre29@gmail.com>
2019-11-05 11:24:56 -0800

    add title

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,13 @@
 import React from 'react'
+
+function Welcome() {
+  return (
+    <div className="container">
+      <h1 className="header">Hello, I'm</h1>
+      <h1 className="header">Mathew Akre</h1>
+      
+    </div>
+  )
+}
+
+export default Welcome;
