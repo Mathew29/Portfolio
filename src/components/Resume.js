e02d3e643decde003888f9359c653e8f4ce0a8b2
Mathew Akre <mwakre29@gmail.com>
2019-12-02 11:16:54 -0800

    add resume/resume wont appear

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Document, Page } from 'react-pdf';
 
-function Resume () {
-  return(
-    <div className='container'>
-      <h1>resume</h1>
-    </div>
-  )
+import pdfFile from './../Resume.pdf';
+
+class Resume extends Component {
+  state = {
+    file: pdfFile,
+    numPages: null,
+    pageNumber: 1,
+  }
+  
+  onFileChange = (event) => {
+    this.setState({
+      file: event.target.files[0],
+    });
+  }
+
+  onDocumentLoadSuccess =({ numPages }) => {
+    this.setState({ numPages });
+  }
+
+  render() {
+    const { file, pageNumber, numPages } = this.state;
+
+    return (
+      <div>
+        <Document
+          file={file}
+          onLoadSuccess={this.onDocumentLoadSuccess}>
+            {/* <Page pageNumber={pageNumber} /> */}
+            {Array.from(
+              new Array(numPages),
+              (el, index) => (
+                <Page
+                key={`page_${index + 1}`}
+                pageNumber={index + 1}
+                />
+              ),
+            )}
+          </Document>
+          {/* <p> Page {pageNumber} of {numPages} </p> */}
+      </div>
+    )
+  }
 }
 
 export default Resume;
