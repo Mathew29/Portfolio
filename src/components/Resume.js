db8cb02d1fdc4d3e4d0b3508aed9ab92467b1be6
Mathew Akre <mwakre29@gmail.com>
2019-12-04 11:48:00 -0800

    add titles

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,6 +4,18 @@ import './Resume.css'
 function Resume() {
   return (
     <div className="container">
+      <div className="title">
+        <h1>Mathew Akre</h1>
+      </div>
+      <div className="summary">
+        <h3>Summary:</h3>
+      </div>
+      <div className="experience">
+        <h3>Experience:</h3>
+      </div>
+      <div className="education">
+        <h3>Education:</h3>
+      </div>
 
     </div>
   )
