74059dd26b386ceb4b835a706785925a91dca770
Mathew Akre <mwakre29@gmail.com>
2019-12-06 11:51:16 -0800

    all jobs entered

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -24,6 +24,22 @@ function Resume() {
           <li>Improved the user-experience (UX) for a social media mobile app that was shown to investors.  The technology used were React Native, Google Places API and Firebase database.</li>
           <li>Redesigned social media mobile app using a custom personal server using MongoDB and React Native.</li>
         </ul>
+        <p>Flying Pie Pizzeria (June 2016 – October 2016)</p>
+        <p>Gresham, Oregon</p>
+        <p>Dishwasher</p>
+        <ul>
+          <li>Cleaned dishes in a timely manner.</li>
+          <li>Created top quality pizza in a fast-paced environment.</li>
+        </ul>
+        <p>Head Basketball Coach (November 2013 – March 2014)</p>
+        <p>Gresham, Oregon</p>
+        <p>Head Coach</p>
+        <ul>
+          <li>Coached an 11-12 year old boys basketball team.</li>
+          <li>Helped develop life skills by teaching teamwork, discipline and fun. </li>
+          <li>Communicated the schedule to parents and players.</li>
+          <li>Developed a strategy and game plan for games.</li>
+        </ul>
       </div>
       <div className="education">
         <h3>Education:</h3>
